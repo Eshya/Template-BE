fd62eb430b1529f9593cd48589f0bb262f600a07
wahono <wahono.gusty12@gmail.com>
2021-08-21 10:41:44 +0700

    speed

--- routes/api/iot/iot.model.js
+++ routes/api/iot/iot.model.js
@@ -31,8 +31,8 @@ exports.getFlock = async (params) => {
 
 exports.update = async (fields, params, data) => {
     const rows = await db.query(`SELECT * FROM perangkat WHERE `+fields+` = ?`, [params]);
-    const query = await db.query(`UPDATE perangkat SET user_id = ?, id_device = ?, nama = ?, mode = ?, status = ?, suhu_aktual = ?, suhu_batas_nyala = ?, suhu_batas_mati = ?, kelembapan_aktual = ?, kelembapan_batas_nyala = ?, kelembapan_batas_mati = ?, jam_mati = ?, menit_mati = ?, detik_mati = ? WHERE `+fields+ ` = ? `, [
-        rows[0].user_id, rows[0].id_device, rows[0].nama, data.mode || rows[0].mode, data.status || rows[0].status, data.suhu_aktual || rows[0].suhu_aktual, data.suhu_batas_nyala || rows[0].suhu_batas_nyala, data.suhu_batas_mati || rows[0].suhu_batas_mati, data.kelembapan_aktual || rows[0].kelembapan_aktual, data.kelembapan_batas_nyala || rows[0].kelembapan_batas_nyala, data.kelembapan_batas_mati || rows[0].kelembapan_batas_mati, data.jam_mati || rows[0].jam_mati, data.menit_mati || rows[0].menit_mati, data.detik_mati || rows[0].detik_mati, params 
+    const query = await db.query(`UPDATE perangkat SET user_id = ?, id_device = ?, nama = ?, mode = ?, status = ?, suhu_aktual = ?, suhu_batas_nyala = ?, suhu_batas_mati = ?, kelembapan_aktual = ?, kelembapan_batas_nyala = ?, kelembapan_batas_mati = ?, jam_mati = ?, menit_mati = ?, detik_mati = ?, speed = ? WHERE `+fields+ ` = ? `, [
+        rows[0].user_id, rows[0].id_device, rows[0].nama, data.mode || rows[0].mode, data.status || rows[0].status, data.suhu_aktual || rows[0].suhu_aktual, data.suhu_batas_nyala || rows[0].suhu_batas_nyala, data.suhu_batas_mati || rows[0].suhu_batas_mati, data.kelembapan_aktual || rows[0].kelembapan_aktual, data.kelembapan_batas_nyala || rows[0].kelembapan_batas_nyala, data.kelembapan_batas_mati || rows[0].kelembapan_batas_mati, data.jam_mati || rows[0].jam_mati, data.menit_mati || rows[0].menit_mati, data.detik_mati || rows[0].detik_mati, data.speed || rows[0].speed, params 
     ])
     let message = 'error in updating data';
     if(!query.affectedRows) return message;
