56b92ab2c1275329615872dbe9bc6aa8634c46c4
wahono <wahono.gusty12@gmail.com>
2021-08-28 19:19:53 +0700

    fixing user signin

--- routes/api/berita/berita.controller.js
+++ routes/api/berita/berita.controller.js
@@ -25,9 +25,14 @@ exports.findPosts = async (req, res, next) => {
 exports.postBycategories = async (req, res, next) => {
     try {
         const get = await axios.get('https://chickin.id/blog/wp-json/wp/v2/posts?categories=14');
-        res.json({
-            data: get.data
-        })
+        // console.log(typeof get.data);
+        for (const x of get.data) {
+            if (x.featured_media ==  2749) return delete x
+            const getImage = await axios.get(`https://chickin.id/blog/wp-json/wp/v2/media/`+ x.featured_media)
+            console.log(getImage.data);
+        }
+        const getImage = await axios.get(`https://chickin.id/blog/wp-json/wp/v2/media/2742`)
+        // console.log(get.data);
     } catch (error) {
         next(error)
     }
@@ -37,8 +42,14 @@ exports.postById = async (req, res, next) => {
     try {
         const id = req.params.id;
         const get = await axios.get('https://chickin.id/blog/wp-json/wp/v2/posts/' + id)
+        const getImage = await axios.get(`https://chickin.id/blog/wp-json/wp/v2/media/` + get.data.featured_media)
         res.json({
-            data: get.data,
+            // data: get.data,
+            id: get.data.id,
+            data: get.data.modified,
+            title: get.data.title.rendered,
+            content: get.data.content.rendered,
+            embed: getImage.data.media_details.sizes.medium,
             message: 'Ok'
         })
     } catch (error) {
@@ -50,12 +61,6 @@ exports.findBanner = async (req, res, next) => {
     try {
         const get = await axios.get(`https://chickin.id/blog/wp-json/wp/v2/posts/2701`)
         const getImage = await axios.get(`https://chickin.id/blog/wp-json/wp/v2/media/`+ get.data.featured_media);
-        // const featuredMedia = 'wp:featuredmedia'
-        // const mergeArray = get.data._embedded.featuredMedia
-        // const getImage = get.map()
-        // console.log({
-            
-        // });
         res.json({
             id: get.data.id,
             data: get.data.modified,
