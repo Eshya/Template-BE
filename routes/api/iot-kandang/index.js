17b29b72a892a724beeb4e5ad3a80152a0b060d5
wahono <wahono.gusty12@gmail.com>
2021-09-07 20:12:19 +0700

    delete iof-kandang

--- routes/api/iot-kandang/index.js
+++ routes/api/iot-kandang/index.js
@@ -5,5 +5,6 @@ const c = require('./iot-kandang.controller')
 
 router.get('/:id', auth, c.findByKandang);
 router.post('/', auth, c.insert);
+router.delete('/:id', auth, c.removeById);
 
 module.exports = router;
