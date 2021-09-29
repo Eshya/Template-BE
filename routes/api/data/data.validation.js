1fb3c2a1f3b0ab9702552bb084ba249928cf4174
wahono <wahono.gusty12@gmail.com>
2021-09-29 10:47:36 +0700

    data endpoint

---
+++ routes/api/data/data.validation.js
@@ -0,0 +1,26 @@
+exports.schema = {
+    day: {
+        isEmpty: false,
+        isString: false
+    },
+    bodyWeight: {
+        isEmpty: false,
+        isString: false
+    },
+    dailyGain: {
+        isEmpty: false,
+        isString: false
+    },
+    adg: {
+        isEmpty: false,
+        isString: false
+    },
+    dailyIntake: {
+        isEmpty: false,
+        isString: false
+    },
+    cumIntake: {
+        isEmpty: false,
+        isString: false
+    }
+}
