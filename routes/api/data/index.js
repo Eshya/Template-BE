743c2c5296faf8c3113970be05e9e0c304e89c64
wahono <wahono.gusty12@gmail.com>
2021-09-29 10:47:36 +0700

    data endpoint

--- routes/api/data/index.js
+++ routes/api/data/index.js
@@ -5,5 +5,11 @@ const c = require('./data.controller');
 const { schema } = require('./data.model');
 
 router.get('/', auth, queryCek, c.findAll);
+router.get('/:id', auth, paramCek, c.findById);
+router.post('/', auth, schemaCek(schema), after, c.insert);
+router.put('/:id', auth, paramCek, c.updateById);
+router.put('/', auth, c.updateWhere);
+router.delete('/', auth, c.remove);
+router.delete('/:id', auth, paramCek, c.removeById);
 
 module.exports = router;
