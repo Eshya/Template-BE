c21321988a5716ba2deed9628c44075e40c34cd5
wahono <wahono.gusty12@gmail.com>
2021-11-05 22:39:05 +0700

    change port default

--- configs/env.conf.js
+++ configs/env.conf.js
@@ -1,4 +1,4 @@
-exports.defaultPort = 3000;
+exports.defaultPort = 8081;
 exports.dbServer = 'localhost:27017';
 exports.projectName = process.env.npm_package_name;
 exports.JWT_SECRET = 'iniR4hasi4loh1!1!-chickin';
