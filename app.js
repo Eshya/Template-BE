e009f3b7297dbcbb2168fdc7a5b2162d3ba4f049
athasamid <athasamid@gmail.com>
2022-01-10 16:59:10 +0700

    Fix conflict

--- app.js
+++ app.js
@@ -20,14 +20,6 @@ const MongoStore = require('connect-mongo')(session);
 const staticFile = 'public';
 const passport = require('passport');
 
-
-const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {}
-const myEmitter = new MyEmitter();
-
-const socketio = require('socket.io')
-
 var app = express();
 
 app.use(logger('dev'));
