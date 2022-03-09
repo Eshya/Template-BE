d261e5f46b0e17ea2b59edac62ba52de008852a3
Robby <robby.alamsyah@finfleet.id>
2022-03-09 13:09:56 +0700

    add endpoint /dashboard/getKelola

--- routes/api/kandang/kandang.model.js
+++ routes/api/kandang/kandang.model.js
@@ -1,4 +1,98 @@
-const {Schema, model} = require('mongoose');
+const { createConnection, Schema, model} = require('mongoose');
+const host = process.env.DB_HOST || '103.31.39.17'
+const dbPort = process.env.DB_PORT || 27018
+const dbName = process.env.DB_NAME_AUTH || 'chickin-auth'
+const user = process.env.DB_USER || 'chickindb'
+const pass = process.env.DB_PASS || 'IniDBch1ck1n'
+const mongoString = process.env.MONGO_CONNECTIONSTRING || `mongodb://${host}:${dbPort}`
+
+const options = {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    dbName,
+    user,
+    pass,
+    auth: {
+        authSource: 'admin'
+    }
+}
+
+const UsersSchema = new Schema({
+    fullname: {
+        type: String,
+        required: true,
+        select: true,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+        select: true,
+    },
+    phoneNumber: {
+        type: Number,
+        required: true
+    },
+    role: {
+        type: Schema.Types.ObjectId,
+        ref: 'Roles', select: true,
+        autopopulate: true  
+    },
+    image: {
+        type: Schema.Types.ObjectId,
+        ref: 'UserImage', select: true,
+        autopopulate: {maxDepth: 1},
+        default: null
+    },
+    noKTP: {
+        type: String,
+        default: null
+    },
+    asalKemitraan: {
+        type: String,
+        default: null
+    },
+    tokenFCM: {
+        type: String,
+        default: null
+    },
+    idFirebase: {
+        type: String,
+        default: null
+    },
+    kelola: [{
+        type: Schema.Types.ObjectId,
+        default: null
+    }],
+    resetPasswordToken: {
+        type: String,
+        default: null,
+        select: false
+    },
+    resetPasswordExpires: {
+        type: Date,
+        default: null,
+        select: false
+    }
+}, {timestamps: true, versionKey: false})
+
+const dbAuth = createConnection(mongoString, options);
+const Users = dbAuth.model('Users', UsersSchema);
+
 const scheme = new Schema({
     kode: {
         type: String,
@@ -33,9 +127,10 @@ const scheme = new Schema({
     },
     createdBy: {
         type: Schema.Types.ObjectId,
-        required: true
+        ref: Users,
+        autopopulate: {maxDepth: 1}
     }
 }, {versionKey: false, timestamps: true})
 scheme.plugin(require('mongoose-autopopulate'));
-scheme.plugin(require('mongoose-delete'), {deletedAt: true, overrideMethods: 'all'})
+scheme.plugin(require('mongoose-delete', {deleteAt: true}))
 module.exports = model('Kandang', scheme, 'kandang');
