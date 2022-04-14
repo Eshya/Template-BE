ad8eea795e7e29eb4201ed3da18e7c7dbf216363
Robby <robby.alamsyah@finfleet.id>
2022-04-14 13:49:24 +0700

    modul dataPenyakit

---
+++ routes/api/data-penyakit/data-penyakit.controller.js
@@ -0,0 +1,118 @@
+const Model = require('./data-penyakit.model')
+const {parseQuery} = require('../../helpers');
+
+const handleQuerySort = (query) => {
+    try{
+      const toJSONString = ("{" + query + "}").replace(/(\w+:)|(\w+ :)/g, (matched => {
+          return '"' + matched.substring(0, matched.length - 1) + '":';
+      }));
+      return JSON.parse(toJSONString);
+    }catch(err){
+      return JSON.parse("{}");
+    }
+}
+
+exports.findAll = async (req, res, next) => {
+    try {
+        const {limit, offset} = parseQuery(req.query);
+        const { name, code, description } = req.query;
+        const sort = handleQuerySort(req.query.sort)
+        const filter = {}
+        if (name) {
+            filter.name = new RegExp(name, 'i')
+        }
+        if (code) {
+            filter.code = new RegExp(code, 'i')
+        }
+        if (description) {
+            filter.description = new RegExp(description, 'i')
+        }
+        const count = await Model.countDocuments(filter)
+        const data = await Model.find(filter).limit(limit).skip(offset).sort(sort)
+        res.json({
+            message: 'Ok',
+            length: count,
+            data: data
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.findById = async (req, res, next) => {
+    try {
+        const results = await Model.findById(req.params.id);
+        res.json({
+            data: results,
+            message: 'Ok'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.insert = async (req, res, next) => {
+    const data = req.body;
+    try {
+        const results = await Model.create(data);
+        res.json({
+            data: results,
+            message: 'Ok'
+        })
+    } catch (error) {
+        next(error)
+    }
+}
+
+exports.updateById = async (req, res, next) => {
+    const id = req.params.id;
+    const data = req.body;
+    try {
+        const results = await Model.findByIdAndUpdate(id, data, {new: true}).exec();
+        res.json({
+            data: results,
+            message: 'Ok'
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.updateWhere = async (req, res, next) => {
+    const {where} = parseQuery(req.query);
+    const data = req.body;
+    try {
+        const results = await Model.updateMany(where, data, {new: true, upsert: false, multiple: false}).exec();
+        res.json({
+            data: results,
+            message: 'Ok'
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.remove = async (req, res, next) => {
+    const {where} = parseQuery(req.query);
+    try {
+        const results = await Model.deleteMany(where).exec();
+        res.json({
+            data: results,
+            message: 'OK'
+        })
+    } catch (error) {
+        next(error);
+    }
+}
+
+exports.removeById = async (req, res, next) => {
+    try {
+        const results = await Model.deleteById(req.params.id).exec();
+        res.json({
+            data: results,
+            message: 'Ok'
+        })
+    } catch (error) {
+        next(error);
+    }
+}
