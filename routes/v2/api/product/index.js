aa22bb5f5a533ab549bee01d9c4edd8fec999929
kanchuts <wahono.gusty12@gmail.com>
2022-04-19 14:17:48 +0700

    endpoint product

---
+++ routes/v2/api/product/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const router = express.Router()
+const c = require('./product.controller')
+const {schema} = require('./product.validation')
+const { auth, queryCek, schemaCek, paramCek, after } = require('../../../helpers')
+const { queryCek } = require('../../../helpers')
+
+router.get('/', auth, queryCek, c.findAll),
+router.get('/', auth, queryCek, c.count)
+router.get('/:id', auth, paramCek, c.findById)
+router.post('/', auth, after, schemaCek(schema), c.insert)
+router.put('/', auth, c.updateWhere)
+router.put('/:id', auth, paramCek, c.updateById)
+router.delete('/', auth, c.remove)
+router.delete('/:id', auth, paramCek, c.removeById)
+
+module.exports = router
