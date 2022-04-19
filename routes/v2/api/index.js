09213eb8223930ed14b56688b892ff9b2c7685be
kanchuts <wahono.gusty12@gmail.com>
2022-04-19 14:19:07 +0700

    change routes

---
+++ routes/v2/api/index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const router = express.Router()
+const {projectName, createError} = require('../../helpers')
+const debug = require('debug')(`${projectName}:api`);
+
+const fs = require('fs')
+const files = fs.readdirSync(__dirname);
+
+files.forEach((endpoint)=> {
+    if (endpoint!='index.js') {
+        debug('v2/'+endpoint)
+        router.use(`/${endpoint}`, require(`./${endpoint}`));
+    }
+});
+
+router.get('/', (req, res, next)=>{
+  res.send(createError(401, `You're not allowed`));
+});
+
+module.exports = router
