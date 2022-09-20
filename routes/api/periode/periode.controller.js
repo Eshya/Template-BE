d17c8ec4a3038eb8b737035360404b9517e680f4
Sanja Avi <avi.sanja@gmail.com>
2022-09-20 16:24:04 +0700

    CT-513 Fix Tanggal Akhir Periode
    - Add tanggal akhir when auto closing

--- routes/api/periode/periode.controller.js
+++ routes/api/periode/periode.controller.js
@@ -608,6 +608,7 @@ exports.autoClosingCultivation = async(req, res, next) => {
 
             periode.isEnd = true;
             chickenShed.isActive = false;
+            periode.tanggalAkhir = Date.now();
             await periode.save();
             await chickenShed.save();
           }
