85c726e1b914b5ce2bec44f675d4937b19e3835f
Sanja Avi <avi.sanja@gmail.com>
2022-10-21 16:03:07 +0700

    Fix some query

--- routes/api/periode/periode.controller.js
+++ routes/api/periode/periode.controller.js
@@ -683,39 +683,34 @@ exports.reActivateChickenSheds = async (req, res, next) => {
 
 exports.weightChart = async (req, res, next) => {
     try {
-      const chickenShed = await Kandang.findById(req.params.id);
-   
       // actual
       const actual = [];
-      if (chickenShed) {
-          const periods = await Model.find({kandang: chickenShed._id}).sort({tanggalMulai: 1});
-          if (periods.length) {
-              const periodeIds = periods.map(periode => periode._id);
-              const dailyActivitiesData = await KegiatanHarian.aggregate([
-                  {$match: {periode: {$in: periodeIds}}},
-                  {$unwind: {'path': '$berat', "preserveNullAndEmptyArrays": true}},
-                  {$group: {
-                      _id: '$_id', 
-                      populasi: {$sum: '$berat.populasi'},
-                      beratTimbang: {$sum: '$berat.beratTimbang'},
-                      periode: { $first: '$$ROOT.periode' }
-                  }}
-              ]);
-      
-              const weightChart = await Promise.map(periods, async(periodeData, index) => {
-                  const dailyActivities = dailyActivitiesData.filter(dailyActivity => dailyActivity.periode.toString() === periodeData._id.toString());
-                  const dailyWeight = !dailyActivities.length ? 0 : dailyActivities.reduce((a, {beratTimbang}) => a + beratTimbang, 0);
-                  const dailyWeightSample = !dailyActivities.length ? 0 : dailyActivities.reduce((a, {populasi}) => a + populasi, 0);
-                  const avgWeight = dailyWeight/dailyWeightSample;
-                  const periodIndex = periods.findIndex(index => index._id === periodeData._id);
-                  return {
-                      actual: avgWeight ? avgWeight : 0,
-                      periode: `Periode ${periodIndex+1}`
-                  }
-              })
-      
-              actual.push(...weightChart);
-          }
+      const periods = await Model.find({kandang: req.params.id}).sort({tanggalMulai: 1}).distinct("_id");
+      if (periods.length) {
+          const dailyActivitiesData = await KegiatanHarian.aggregate([
+              {$match: {periode: {$in: periods}}},
+              {$unwind: {'path': '$berat', "preserveNullAndEmptyArrays": true}},
+              {$group: {
+                  _id: '$_id', 
+                  populasi: {$sum: '$berat.populasi'},
+                  beratTimbang: {$sum: '$berat.beratTimbang'},
+                  periode: { $first: '$$ROOT.periode' }
+              }}
+          ]);
+  
+          const weightChart = await Promise.map(periods, async(periodeData) => {
+              const dailyActivities = dailyActivitiesData.filter(dailyActivity => dailyActivity.periode.toString() === periodeData.toString());
+              const dailyWeight = !dailyActivities.length ? 0 : dailyActivities.reduce((a, {beratTimbang}) => a + beratTimbang, 0);
+              const dailyWeightSample = !dailyActivities.length ? 0 : dailyActivities.reduce((a, {populasi}) => a + populasi, 0);
+              const avgWeight = dailyWeight/dailyWeightSample;
+              const periodIndex = periods.findIndex(index => index._id === periodeData);
+              return {
+                  actual: avgWeight || 0,
+                  periode: `Periode ${periodIndex+1}`
+              }
+          })
+  
+          actual.push(...weightChart);
       }
    
       return res.json({ data: actual, message: 'success', status: 200  });
